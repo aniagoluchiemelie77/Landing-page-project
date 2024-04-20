53e71f6de7ce56006d839a25f8184600ebfe6c93
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2024-04-20 18:47:21 +0100

    Latest commit

---
+++ Landing p/landing.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//selecting the various sections
+const sec1 = document.querySelector('.sec1');
+const sec2 = document.querySelector('.sec2');
+const sec3 = document.querySelector('.sec3');
+const sec4 = document.querySelector('.sec4');
+const sec5 = document.querySelector('.sec5');
+const sec6 = document.querySelector('.sec6');
+const sec7 = document.querySelector('.sec7');
+const sec8 = document.querySelector('.sec8');
