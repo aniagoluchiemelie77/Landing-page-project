f3402ddb1cd3febc519c3d792fa2f03362cf3aa1
Aniagolu Chiemelie ® <123953064+aniagoluchiemelie77@users.noreply.github.com>
2024-05-04 16:28:19 +0100

    latest Commit5

--- Landing p/landing.js
+++ Landing p/landing.js
@@ -9,27 +9,31 @@ const sec5 = document.querySelector('.sec5');
 const sec6 = document.querySelector('.sec6');
 const sec7 = document.querySelector('.sec7');
 const sec8 = document.querySelector('.sec8');
-const pic1 = document.getElementById('pic1'); 
-const pic2 = document.getElementById('pic2'); 
-const pic3 = document.getElementById('pic3'); 
-const pic4 = document.getElementById('pic4'); 
-  
 
-  
-function animate (element) { 
-    let elementWidth = element.offsetWidth; 
-    let parentWidth = element.parentElement.offsetWidth; 
-    let flag = 0; 
-  
-    setInterval(() => { 
-        element.style.marginLeft = --flag + "px"; 
-  
-        if (elementWidth == -flag) { 
-            flag = parentWidth; 
-        } 
-    }, 10); 
-} 
-animate(pic1); 
-animate(pic2); 
-animate(pic3); 
-animate(pic4);
+//selecting images 
+const pic1 = document.querySelector('.sec4__div3__pic1');
+const pic2 = document.querySelector('.sec4__div3__pic2');
+const pic3 = document.querySelector('.sec4__div3__pic3');
+const pic4 = document.querySelector('.sec4__div3__pic4');
+
+const imgBox = document.getElementById('imagebox');
+const imgs = document.querySelectorAll('.sec4img');
+const images = [pic1, pic2, pic3, pic4];
+
+const heightImgBox = imgBox.offsetTop;
+const widthImgBox = imgBox.offsetWidth;
+console.log(widthImgBox);
+const displayImg = setTimeout(function(array){
+    array.forEach(function() {
+        for( let i = 0; i < array.length; i++){
+           return array[i]
+        }
+    } )
+}, 3000);
+const newChild = displayImg(images);
+const changeImg = function(){
+    if (window.innerHeight === heightImgBox && window.innerWidth === widthImgBox && window.innerWidth <= 640){
+        imgBox.replaceChild(imgs, newChild);  
+    }
+}
+changeImg();
